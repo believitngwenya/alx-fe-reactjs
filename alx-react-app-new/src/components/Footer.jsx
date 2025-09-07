c2bb20b2fb78d24b582a593d42ad7fb9fceab5e5
Noxolo Believit Ngwenya <believitngwenya@outlook.com>
2025-09-07 14:11:02 +0200

    Added inline styling

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer>
-      <p>© 2023 City Lovers</p>
+    <footer style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+      <p><span style={{ fontWeight: 'bold',color:'purple' }}>© 2023 City Lovers</span></p>
     </footer>
   );
 }
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,12 @@
 function MainContent() {
   return (
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+    <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+      <p><span style={{ fontWeight: 'bold',color:'red' }}>I love</span>
+       <span style={{ fontWeight: 'bold',color:'navy' }}>to visit</span>
+         <span style={{ fontWeight: 'bold',color:'purple' }}>New York,</span>
+          <span style={{ fontWeight: 'bold',color:'pink' }}>Paris,</span>
+           <span style={{ fontWeight: 'bold',color:'red' }}>and</span>
+            <span style={{ fontWeight: 'bold',color:'orange' }}> Tokyo.</span></p>
     </main>
   );
 }
