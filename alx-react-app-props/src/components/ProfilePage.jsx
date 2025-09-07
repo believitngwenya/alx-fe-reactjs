f7d6d092672d33fff7aee1d203a007bd605e52d1
Noxolo Believit Ngwenya <believitngwenya@outlook.com>
2025-09-07 15:11:10 +0200

    Refactoring

---
+++ alx-react-app-props/src/components/ProfilePage.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import UserInfo from "./UserInfo";
+
+function ProfilePage() {
+  return (
+    <div>
+      <h1>My Profile</h1>
+      <UserInfo />
+    </div>
+  );
+}
+
+export default ProfilePage;
---
+++ alx-react-app-props/src/components/UserInfo.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import UserDetails from "./UserDetails";
+
+function UserInfo() {
+  return (
+    <section>
+      <h2>User Info</h2>
+      <UserDetails />
+    </section>
+  );
+}
+
+export default UserInfo;
