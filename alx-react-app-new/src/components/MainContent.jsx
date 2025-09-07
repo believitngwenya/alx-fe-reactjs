ab6370afda0fd6779f3fd2206b681c24b65b134a
Noxolo Believit Ngwenya <believitngwenya@outlook.com>
2025-09-07 14:16:09 +0200

    Added inline styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,12 +1,7 @@
 function MainContent() {
   return (
     <main style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
-      <p><span style={{ fontWeight: 'bold',color:'red' }}>I love</span>
-       <span style={{ fontWeight: 'bold',color:'navy' }}>to visit</span>
-         <span style={{ fontWeight: 'bold',color:'purple' }}>New York,</span>
-          <span style={{ fontWeight: 'bold',color:'pink' }}>Paris,</span>
-           <span style={{ fontWeight: 'bold',color:'red' }}>and</span>
-            <span style={{ fontWeight: 'bold',color:'orange' }}> Tokyo.</span></p>
+      <p><span style={{ fontWeight: 'bold',color:'red' }}>I love to visit New York, Paris, and Tokyo.</span></p>
     </main>
   );
 }
